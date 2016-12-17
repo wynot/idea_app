b1a45e3c98807754168b778554eb62b2453787d1
Will Young <info@willyoung.me>
2016-12-17 00:16:53 -0500

    adding ideas and opportunities

---
+++ app/views/opportunities/_opportunity.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! opportunity, :id, :name, :created_at, :updated_at
+json.url opportunity_url(opportunity, format: :json)
---
+++ db/migrate/20161217051517_create_opportunities.rb
@@ -0,0 +1,9 @@
+class CreateOpportunities < ActiveRecord::Migration
+  def change
+    create_table :opportunities do |t|
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
